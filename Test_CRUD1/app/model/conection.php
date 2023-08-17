3b2c11876cb1343429baf1464cb8e41c898428b3
kulumbo10 <124624241+kulumbo10@users.noreply.github.com>
2023-08-17 07:21:56 -0400

    20230817_0721

---
+++ Test_CRUD1/app/model/conection.php
@@ -0,0 +1,19 @@
+<?php
+
+//class responsael por estabelecer conexao com o banco de dados
+class Conection{
+    private static $instance;
+    public static function getcon(){
+
+        if(!isset(self::$instance)):
+            self::$instance = new PDO/(mysql: host = localhost dbname = pdo; charset = utf8, 'root','');
+        else:
+
+        return self::$instance;
+        // se nao usar o endif vai dar erro na declaracao do return
+        endif;
+
+    }
+  
+
+}
